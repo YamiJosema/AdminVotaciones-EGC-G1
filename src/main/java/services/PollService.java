5c198b9adefb387d5e3e147d49cd35ce0421bc28
jospalhid <Es25Alba>
2017-12-17 17:06:41 +0100

    Metodos CRUD Poll
    El metodo Create recive un censo. No hay metodo Delete debido a que no
    se podran borrar las encuestas una vez creadas.
    
    Issue #16, #5

--- src/main/java/services/PollService.java
+++ src/main/java/services/PollService.java
@@ -5,10 +5,10 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.PollRepository;
-import security.LoginService;
-import security.UserAccount;
+import domain.Census;
 import domain.Poll;
 
 @Service
@@ -30,60 +30,25 @@ public class PollService {
 
 	// Simple CRUD methods -----------------------------------------------------
 
-	public Poll create() {
-		Poll result;
-		result = new Poll();
+	public Poll create(Census census) {
+		Poll result = new Poll();
+		result.setCensus(census);
 		return result;
-	}
-
-	public Collection<Poll> findByPrincipal() {
-		final Collection<Poll> result;
-		UserAccount userAccount;
-
-		userAccount = LoginService.getPrincipal();
-		// result = this.pollRepository.findByUserAccount(userAccount);
-
-		return this.findAll();
 	}
 
 	public Poll findOne(final int pollId) {
-		Poll result;
-
-		result = this.pollRepository.findOne(pollId);
-
-		return result;
-	}
-
-	public Poll findOneToEdit(final int PollId) {
-		Poll result;
-
-		result = this.findOne(PollId);
-		// this.checkPrincipal(result);
-
-		return result;
+		return this.pollRepository.findOne(pollId);
 	}
 
 	public Collection<Poll> findAll() {
 		return this.pollRepository.findAll();
 	}
 
-	public void save(final Poll Poll) {
-		// this.checkPrincipal(Poll);
-		this.pollRepository.save(Poll);
-	}
-
-	public void delete(final Poll Poll) {
-		// this.checkPrincipal(Poll);
-		this.pollRepository.delete(Poll);
+	public Poll save(final Poll poll) {
+		Assert.notNull(poll);
+		return this.pollRepository.save(poll);
 	}
 
 	// Other business methods --------------------------------------------------
-
-	// protected void checkPrincipal(final Poll Poll) {
-	// Customer customer;
-	//
-	// customer = this.customerService.findByPrincipal();
-	// Assert.isTrue(Poll.getCustomer().equals(customer));
-	// }
 
 }
