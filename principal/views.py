dc1451d4927d50c8fa18c29c4cd39d91ffbd2fa8
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2018-01-04 19:11:03 +0100

    Creación de una votación
    Nueva funcionalidad que permite crear votaciones a partir de los campos
    title, description, startDate, endDate y census. El resto de atributos
    se inicializa de forma automatica. La nueva vista consiste en un
    formulario con inputs para los 4 primeros campos y un select para el
    campo census.
    
    Issue #55

--- principal/views.py
+++ principal/views.py
@@ -1,4 +1,5 @@
 from principal.models import *
+from principal.forms import *
 from django.shortcuts import render_to_response, get_list_or_404
 #Para autenticacion
 from django.contrib.auth import authenticate
@@ -12,3 +13,13 @@ from django.template import RequestContext
 def inicio(request):
     votaciones = Poll.objects.all()
     return render_to_response("lista.html",{"votaciones":votaciones})
+
+def nueva_votacion(request):
+    if request.method=='POST':
+        formulario = PollForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = PollForm()
+    return render_to_response('nuevavotacion.html',{'formulario':formulario}, context_instance=RequestContext(request))
