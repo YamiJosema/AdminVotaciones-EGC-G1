16fe4c0d7c213826c0fd7303bbaa03487435566d
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2018-01-07 14:49:33 +0100

    Formulario para añadir preguntas
    Una vez que accedamos al link de 'Nueva Pregunta' en la lista de
    votaciones futuras, se nos permitirá rellenar los campos "Title" y
    "Description" y marcar las opciones "Optional" y "Multiple". El id de la
    votación estará en la url. Una vez guardado, se nos redirigirá a la
    pantalla de inicio. 
    
    Closes #59

--- principal/views.py
+++ principal/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render_to_response, get_list_or_404
 from django.contrib.auth import authenticate
 #Para usar Formularios
 #from principal.forms import *
-from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 
