4edb1777b1d95325f19a074a61b072e2992a79b3
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2018-01-07 15:21:11 +0100

    Formulario para añadir preguntas
    Una vez que accedamos al link de 'Nueva Pregunta' en la lista de
    votaciones futuras, se nos permitirá rellenar los campos "Title" y
    "Description" y marcar las opciones "Optional" y "Multiple". El id de la
    votación estará en la url. Una vez guardado, se nos redirigirá a la
    pantalla de inicio. 
    
    Closes #59

--- principal/views.py
+++ principal/views.py
@@ -30,3 +30,16 @@ def votaciones_futuras(request):
     now = datetime.today().date()    
     votaciones = Poll.objects.filter(startDate__range=[now, "2050-01-31"])
     return render_to_response("lista.html",{"votaciones":votaciones, "preguntas":True})
+
+def nueva_pregunta(request, poll_id):
+    if request.method=='POST':
+        formulario = QuestionForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            poll = Poll.objects.get(id=poll_id)
+            Question.objects.create(title=formulario.cleaned_data['title'], description=formulario.cleaned_data['description'], optional=formulario.cleaned_data['optional'], multiple=formulario.cleaned_data['multiple'], poll=poll)
+#             formulario.save()
+#             return HttpResponseRedirect('/nuevapregunta/'+poll_id)
+            return HttpResponseRedirect('/')
+    else:
+        formulario = QuestionForm()
+    return render_to_response('nuevapregunta.html',{'formulario':formulario}, context_instance=RequestContext(request))
